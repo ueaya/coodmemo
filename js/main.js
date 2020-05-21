d00ab131a38bee2a8b77481c7ce55b8d61d15f68
ueaya <ayahauekaseda@gmail.com>
2020-05-21 12:30:40 +0900

    add css js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿
+$(function() {
+  $('.tab_btn').click(function() {
+
+    $('.tab_btn').removeClass('is-active-btn');
+    $('.tab-item').removeClass('is-active-item');
+    $($(this).addClass('is-active-btn'));
+    $($(this).attr('href')).addClass('is-active-item');
+  });
+});
